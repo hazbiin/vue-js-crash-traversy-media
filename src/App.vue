5505dab777c940873da7f6b3f5a8809474025c53
hazbiin <hzbi717@gmail.com>
2025-06-09 09:38:10 +0530

    coversion to composition API long form

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,35 @@
 <script>
+  import { ref } from 'vue';
+
   export default {
-    data() {
-      return {
-        name: "John Doe",
-        boolStatus: true,
-        status: "pending",
-        tasks: ['Task One', 'Task Two', 'Task Three'],
-        link: 'https://google.com'
-      }
-    },
-    methods: {
-      toggleStatus() {
-        if(this.status === "active") {
-          this.status = 'pending';
-        }else if(this.status === 'pending') {
-          this.status = 'inactive';
+    setup() {
+      const name = ref('John Doe');
+      const status = ref('active');
+      const tasks = ref(['Task One', 'Task Two', 'Task Three']);
+
+      const toggleStatus = () => {
+        if(status.value === "active"){
+          status.value = "pending";
+        }else if(status.value === "pending"){
+          status.value = 'inactive';
         }else {
-          this.status ='active'
+          status.value = 'active';
         }
+      }
+
+      return {
+        name,
+        status,
+        tasks,
+        toggleStatus
       }
     }
   }
-
 </script>
 
 
 <template>
   <h1>{{ name }}</h1>
-  <h2 v-if=boolStatus>rendered by boolean data</h2>
 
   <p v-if="status === 'active' ">User is active</p>
   <p v-else-if="status === 'pending'">User is pending</p>
@@ -38,8 +40,6 @@
     <li v-for="task in tasks" :key="task"> {{ task }}</li>
   </ul>
 
-  <!-- <a v-bind:href="link">Click for Google</a> -->
-  <a :href="link">Click for Google</a>
   <br/>
   <!-- <button v-on:click="toggleStatus">Change Status</button> -->
   <button @click="toggleStatus">Change Status</button>
